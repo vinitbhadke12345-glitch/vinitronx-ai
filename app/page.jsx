6a774321ab5e89b86cc5141e59b187843e046122
vinitbhadke12345-glitch <vinitbhadke12345@gmail.com>
2026-04-24 22:06:04 +0530

    Updated app/page.jsx with VinitronxAI component.

--- app/page.jsx
+++ app/page.jsx
@@ -1,2 +1,22 @@
-<h1>Welcome to Vinitronx AI</h1>
-<p>This is the home page of the application.</p>
+import React from 'react';
+import Bg from './Bg';
+import Nav from './Nav';
+import Home from './Home';
+import Chat from './Chat';
+import Features from './Features';
+import About from './About';
+
+const VinitronxAI = () => {
+    return (
+        <div>
+            <Bg />
+            <Nav />
+            <Home />
+            <Chat />
+            <Features />
+            <About />
+        </div>
+    );
+};
+
+export default VinitronxAI;
